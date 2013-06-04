7e9447e392b89cc561caa001804d969e04f0ff61
Robert Brouard <robert.brouard@hsoftware.com>
2013-06-04 14:12:34 +0200

    Modified Version upgrade template

--- supersql/src/main/resources/templates/sqlserver/upgradeVersion.sql
+++ supersql/src/main/resources/templates/sqlserver/upgradeVersion.sql
@@ -2,7 +2,7 @@
 -- IF THE VERSION IS NOT THE EXPECTED, YOU SHOULD MANUALLY STOP THE SCRIPT (it loops indefinitely)
 if not exists (select 1 from Version where databaseVersion like '${previous}' and component = '${component}')
 begin
-	raiserror 50000 'Incorrect database version, please check your version before running this script!'
+	RAISERROR ('Incorrect database version, please check your version before running this script!', 17, 1);
 	while 1 = 1
 	begin
 		WaitFor Delay '00:00:05'
