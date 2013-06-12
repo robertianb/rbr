9ac9379c1e212347aef5514eba406228bce3c953
Robert Brouard <robert.brouard@hsoftware.com>
2013-06-12 18:28:53 +0200

    modifyColumn.sql sybase

--- supersql/src/main/resources/templates/sybase/modifyColumn.sql
+++ supersql/src/main/resources/templates/sybase/modifyColumn.sql
@@ -1,13 +1,13 @@
 -- Table ${tableName} : modify column ${columnName} 's type from ${columnPreviousType} to ${columnType}
-declare @username varchar(200);
+declare @username varchar(200)
+declare @userid int
 set @username = (select user_name())
-declare @userid int;
 set @userid = (select user_id(@username))
 
-declare @details_count int;
+declare @details_count INT
 set @details_count = (select count(*) from syscolumns where upper(name)=upper('${columnName}') and id in (select id from sysobjects where upper(name) = upper('${tableName}') and uid=@userid))
-if @details_count =1
+if @details_count = 1
 begin
-  execute ('alter table ${tableName} alter column ${columnName}  ${columnType}')
+  execute ('alter table ${tableName} modify ${columnName}  ${columnType}')
 end
 go
