51cec0f4fd812ea7879c32f6fa7a39800fb9e5db
Robert Brouard <robert.brouard@hsoftware.com>
2014-04-16 12:23:57 +0200

    H2 rename column template fix

--- supersql/src/main/resources/templates/h2/renameColumnWithoutDefaultValue.sql
+++ supersql/src/main/resources/templates/h2/renameColumnWithoutDefaultValue.sql
@@ -1,2 +1,2 @@
 -- Table ${tableName} : rename column ${previous} to ${next}
-alter table ${tableName} rename column ${previous} to ${next}
+alter table ${tableName} alter column ${previous} rename to ${next}
