d68290f78d7aca5d15598951259ebd895bef367c
Robert Brouard <robert.brouard@hsoftware.com>
2014-04-29 15:26:00 +0200

    Sybase : fixed template deleteColumnWithDefaultValue.sql

--- supersql/src/main/resources/templates/sybase/deleteColumnWithDefaultValue.sql
+++ supersql/src/main/resources/templates/sybase/deleteColumnWithDefaultValue.sql
@@ -1,29 +1,13 @@
--- Table ${tableName} : drop column ${columnName} and constraints
+-- Table ${tableName} : drop column ${columnName}
 declare @username varchar(200)
-DECLARE @colname VARCHAR(100), @tblname VARCHAR(100), @cmd VARCHAR(1000)
-declare @username varchar(200)
+declare @userid int
 set @username = (select user_name())
-declare @userid INT
 set @userid = (select user_id(@username))
 
-SET @colname = '${columnName}'
-SET @tblname = '${tableName}'
-SET @cmd = 'ALTER TABLE ' + @tblname + ' DROP CONSTRAINT ' +
-(SELECT so.name
-FROM sysobjects so
-JOIN sysconstraints con ON so.id = con.constrid
-JOIN syscolumns col ON col.colid = con.colid
-WHERE object_name(con.tableid) = @tblname 
-AND uid=@userid
-AND so.type = 'R'
-AND col.name = @colname)
-
 declare @details_count INT
-set @details_count = (select count(*) from syscolumns where upper(name)=upper(@colname) and id in (select id from sysobjects where upper(name) = upper(@tblname) and uid=@userid))
-if @details_count > 0
+set @details_count = (select count(*) from syscolumns where upper(name)=upper('${columnName}') and id in (select id from sysobjects where upper(name) = upper('${tableName}') and uid=@userid))
+if @details_count = 1
 begin
-  print @cmd
-  execute (@cmd)
-  execute ('alter table '+@tblname+' drop '+@colname)
+  execute ('alter table ${tableName} drop ${columnName}')
 end
 go
